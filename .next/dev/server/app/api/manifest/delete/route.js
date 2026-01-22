b0d526dcdba1d1112101181957d73002c0db6e0f
diwamln <diwamaulana04@gmail.com>
2026-01-22 15:09:25 +0700

    chore: remove build artifacts and sync changes

--- .next/dev/server/app/api/manifest/delete/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/manifest/delete/route.js")
-R.c("server/chunks/bf143_16c12f33._.js")
-R.c("server/chunks/[root-of-the-server]__01eabdce._.js")
-R.c("server/chunks/1d433__next-internal_server_app_api_manifest_delete_route_actions_362514a0.js")
-R.m("[project]/webui-devops/Naratel-Devops-Dashboard/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/webui-devops/Naratel-Devops-Dashboard/src/app/api/manifest/delete/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/webui-devops/Naratel-Devops-Dashboard/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/webui-devops/Naratel-Devops-Dashboard/src/app/api/manifest/delete/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/chunks/_next-internal_server_app_api_manifest_delete_route_actions_9e70faf6.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/manifest/delete/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_manifest_delete_route_actions_9e70faf6.js.map
